655752efc99d4c5b94998b779a36c235c01b17a2
mariozivkovic <mario.zivkovic.znr@gmail.com>
2023-03-27 14:47:50 +0200

    Dorađivanje

--- OtpremnaStanicaAPP/src/main/java/otpremnastanica/view/ProzorZaposlenik.java
+++ OtpremnaStanicaAPP/src/main/java/otpremnastanica/view/ProzorZaposlenik.java
@@ -51,7 +51,7 @@ public class ProzorZaposlenik extends javax.swing.JFrame {
         
     }
     
-    private void ucitaj(){
+      private void ucitaj(){
         
         DefaultListModel<Zaposlenik> m = new DefaultListModel<>();
         m.addAll(obrada.read(txtUvjet.getText(), chbTraziOdPocetkaImena.isSelected()));
@@ -222,7 +222,6 @@ public class ProzorZaposlenik extends javax.swing.JFrame {
                     .addComponent(chbTraziOdPocetkaImena, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel1)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(btnDodaj, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -239,16 +238,20 @@ public class ProzorZaposlenik extends javax.swing.JFrame {
                         .addComponent(txtOib)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(btnDovuciOib))
-                    .addComponent(txtEmail))
+                    .addComponent(txtEmail)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(36, 36, 36)
+                        .addComponent(jLabel1)))
                 .addGap(41, 41, 41))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(35, 35, 35)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addGap(1, 1, 1)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
                     .addComponent(txtUvjet, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(8, 8, 8)
